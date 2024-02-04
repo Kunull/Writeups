ae7dca9cba1c3be0f89a1f51136f3c276fab5aad
Kunal Walavalkar <kunalw2002@gmail.com>
2024-02-04 13:39:14 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       
