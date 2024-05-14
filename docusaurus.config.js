22e70438e3d056a08499e7516324b6d2d809797b
Kunal Walavalkar <kunalw2002@gmail.com>
2024-05-14 10:20:07 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Knign', // Usually your GitHub org/user name.
+  organizationName: 'Kunull', // Usually your GitHub org/user name.
   projectName: 'Write-ups', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
